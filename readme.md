f7a012c9c6702b52c384ba1d8d62fc43f97a03ec
mfaiza <mohamed-iheb.faiza@etu.umontpellier.fr>
2017-01-02 16:15:27 +0100

    added graphical interface

--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 #Weathy
-![GitHub Logo](Weathy_logo.png)
+![Logo](Weathy_logo.png)
+
 A simple Python Weather Program using python requests library and openweathermap API.
 
 
 ##Dependencies
-This app relies on python requests library.
+This app relies on python requests and appJar libraries.
 ###Install
 $ pip install requests
+$ pip install appJar
--- weathy.py
+++ weathy.py
@@ -1,8 +1,50 @@
+import os
 import requests
+from appJar import gui
 
 APIKey = "APPID=affd60b454bf07754ead0c6265738496"
-City = input("Enter City : ")
-request = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+City+"&"+APIKey)
-dictionnary = eval(request.text)
-print("status : ",dictionnary["weather"][0]["description"])
-print("temp : ",dictionnary["main"]["temp"])
+
+def update(btn) :
+    City = app.getLabel("currentcity")
+    request = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+City+"&"+APIKey)
+    dictionnary = eval(request.text)
+    Description = dictionnary["weather"][0]["description"]
+    Temp = dictionnary["main"]["temp"]
+    app.setLabel("currentcity",City)
+    app.setLabel("description", Description)
+    app.setLabel("temp",Temp)
+
+def loadCurrentCity() :
+    if os.path.exists("currentcity") :
+        current = open("currentcity",'r')
+        return current.read()
+    return None
+
+def changeCity(btn) :
+    City = app.getEntry("Change City")
+    current = open("currentcity",'w')
+    current.write(City)
+    app.removeEntry("Change City")
+    app.removeButton("Change")
+    app.addLabel("currentcity", City)
+    app.addLabel("description", text=None)
+    app.addLabel("temp", text=None)
+    app.addButton("Update", update)
+    update(0)
+
+app = gui()
+app.setBg("White")
+app.addImage("logo", "Weathy_logo.png")
+
+if loadCurrentCity() :
+    app.addLabel("currentcity", loadCurrentCity())
+    app.addLabel("description", text=None)
+    app.addLabel("temp", text=None)
+    app.addButton("Update", update)
+    update(0)
+else :
+    app.addLabelEntry("Change City")
+    app.addButton("Change", changeCity)
+
+
+app.go()
